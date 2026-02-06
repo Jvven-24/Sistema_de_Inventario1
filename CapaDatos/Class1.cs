74bd6f7a8d24ef9c543d95496bf54142b19f4634
Jvven <jevenssenmartinezm24@gmail.com>
2026-02-06 17:52:03 -0400

    Add project files.

---
+++ CapaDatos/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        // Cadena de conexión
+        private string cadenaConexion = "Data Source=.;Initial Catalog=Sistema-Inventario;Integrated Security=True";
+
+        // Propiedad para obtener la conexión
+        public SqlConnection ObtenerConexion()
+        {
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+            try
+            {
+                conexion.Open();
+                return conexion;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al conectar con la base de datos: " + ex.Message);
+            }
+        }
+
+        // Método para cerrar la conexión
+        public void CerrarConexion(SqlConnection conexion)
+        {
+            if (conexion != null && conexion.State == System.Data.ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
